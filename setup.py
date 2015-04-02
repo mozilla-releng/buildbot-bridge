117e2858cf3738b53c0384dd1413ba6bb1550e13
Ben Hearsum <bhearsum@mozilla.com>
2015-04-02 09:56:49 -0400

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="bbb",
+    version="0.1",
+    description="Buildbot <-> Taskcluster Bridge",
+    author="Mozilla Release Engineering",
+    packages=["bbb"],
+    install_requires=[
+        "arrow",
+        "taskcluster",
+        "sqlalchemy",
+        "requests",
+        "mozillapulse",
+        "redo",
+    ],
+)
