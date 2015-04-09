99213b594069ebed039d6e31cc6888873b3ed910
Ben Hearsum <bhearsum@mozilla.com>
2015-04-09 10:46:04 -0400

    Enhance BBBDb and add BuildbotDb

--- bbb/servicebase.py
+++ bbb/servicebase.py
@@ -1,8 +1,52 @@
+import json
+
+import arrow
+from mozillapulse.config import PulseConfiguration
+from mozillapulse.consumers import GenericConsumer
 import sqlalchemy as sa
+from taskcluster import Queue
 
 import logging
 log = logging.getLogger(__name__)
 
+
+def parseDateString(datestring):
+    """
+    Parses a date string like 2015-02-13T19:33:37.075719Z and returns a unix epoch time
+    """
+    return arrow.get(datestring).timestamp
+
+
+def create_sourcestamp(db, sourcestamp={}):
+    q = sa.text("""INSERT INTO sourcestamps
+                (`branch`, `revision`, `patchid`, `repository`, `project`)
+            VALUES
+                (:branch, :revision, NULL, :repository, :project)
+            """)
+    branch = sourcestamp.get('branch')
+    revision = sourcestamp.get('revision')
+    repository = sourcestamp.get('repository', '')
+    project = sourcestamp.get('project', '')
+
+    r = db.execute(q, branch=branch, revision=revision, repository=repository, project=project)
+    ssid = r.lastrowid
+    log.info("Created sourcestamp %s", ssid)
+
+    # TODO: Create change objects, files, etc.
+    return ssid
+
+
+def create_buildset_properties(db, buildsetid, properties):
+    q = sa.text("""INSERT INTO buildset_properties
+            (`buildsetid`, `property_name`, `property_value`)
+            VALUES
+            (:buildsetid, :key, :value)
+            """)
+    props = {}
+    props.update(((k, json.dumps((v, "bbb"))) for (k, v) in properties.iteritems()))
+    for key, value in props.items():
+        db.execute(q, buildsetid=buildsetid, key=key, value=value)
+        log.info("Created buildset_property %s=%s", key, value)
 
 def create_tasks_table(db):
     meta = sa.MetaData(db)
@@ -18,21 +62,95 @@ def create_tasks_table(db):
     return tasks
 
 
-class ServiceBase(object):
-    """A base for all BBB services that manages access to the buildbot db,
-       bbb db, and taskcluster."""
-    def __init__(self, bbb_db, buildbot_db, tc_credentials):
-        self.bbb_db = sa.create_engine(task_db)
-        self.buildbot_db = sa.create_engine(task_db)
-        self.tc_queue = Queue(tc_credentials)
-
+class BBBDb(object):
+    def __init__(self, uri):
+        self.db = sa.create_engine(uri)
         self.tasks_table = create_tasks_table(self.bbb_db)
 
-    def deleteBuildrequest(self, brid):
+    def getTask(self, taskId):
+        return self.tasks_table.select(self.tasks_table.c.taskId == taskId).execute().fetchone()
+
+    def createTask(self, taskId, runId, brid, createdDate):
+        self.tasks_table.insert().values(
+            taskId=taskId,
+            runId=runId,
+            buildrequestId=brid,
+            createdDate=createdDate,
+            processedDate=arrow.now().timestamp,
+        ).execute()
+
+    def deleteBuildRequest(self, brid):
         self.tasks_table.delete(self.tasks_table.c.buildrequestId == brid).execute()
 
     def updateRunId(self, brid, runId):
         self.tasks_table.update(self.tasks_table.c.buildrequestId == brid).values(runId=runId).execute()
+
+
+class BuildbotDb(object):
+    def __init__(self, uri):
+        self.db = sa.create_engine(uri)
+
+    def injectTask(self, taskId, task):
+        payload = task["payload"]
+        # Create a sourcestamp if necessary
+        sourcestamp = payload.get('sourcestamp', {})
+
+        sourcestampid = create_sourcestamp(self.db, sourcestamp)
+
+        # Create a buildset
+        q = sa.text("""INSERT INTO buildsets
+            (`external_idstring`, `reason`, `sourcestampid`, `submitted_at`, `complete`, `complete_at`, `results`)
+            VALUES
+            (:idstring, :reason, :sourcestampid, :submitted_at, 0, NULL, NULL)""")
+
+        # TODO: submitted_at should be now, or the original task?
+        # using orginal task's date for now
+        submitted_at = parseDateString(task['created'])
+        r = self.db.execute(
+            q,
+            idstring="taskId:{}".format(taskId),
+            reason="Created by BBB for task {0}".format(taskId),
+            sourcestampid=sourcestampid,
+            submitted_at=submitted_at,
+        )
+
+        buildsetid = r.lastrowid
+        log.info("Created buildset %i", buildsetid)
+
+        # Create properties
+        properties = payload.get('properties', {})
+        # Always create a property for the taskId
+        properties['taskId'] = taskId
+        create_buildset_properties(self.db, buildsetid, properties)
+
+        # Create the buildrequest
+        buildername = payload['buildername']
+        priority = payload.get('priority', 0)
+        q = sa.text("""INSERT INTO buildrequests
+                (`buildsetid`, `buildername`, `submitted_at`, `priority`,
+                    `claimed_at`, `claimed_by_name`, `claimed_by_incarnation`,
+                    `complete`, `results`, `complete_at`)
+                VALUES
+                (:buildsetid, :buildername, :submitted_at, :priority, 0, NULL, NULL, 0, NULL, NULL)""")
+        log.info(q)
+        r = self.db.execute(
+            q,
+            buildsetid=buildsetid,
+            buildername=buildername,
+            submitted_at=submitted_at,
+            priority=priority
+        )
+        log.info("Created buildrequest %s: %i", buildername, r.lastrowid)
+        return r.lastrowid
+
+
+class ServiceBase(object):
+    """A base for all BBB services that manages access to the buildbot db,
+       bbb db, and taskcluster."""
+    def __init__(self, bbb_db, buildbot_db, tc_credentials):
+        self.bbb_db = BBBDb(bbb_db)
+        self.buildbot_db = BuildbotDb(buildbot_db)
+        self.tc_queue = Queue(tc_credentials)
 
     def start(self):
         raise NotImplementedError()
