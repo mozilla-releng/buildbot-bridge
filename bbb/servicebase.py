6aaee29e44e2c93d6478abd2f1451bd00b9f3714
Ben Hearsum <bhearsum@mozilla.com>
2015-04-24 12:23:04 -0400

    Add TODO.

--- bbb/servicebase.py
+++ bbb/servicebase.py
@@ -85,6 +85,8 @@ class BuildbotDb(object):
         return self.db.execute(sa.text("select * from buildrequests where id=:brid"), brid=brid).fetchone()
 
     def getBuildRequests(self, buildnumber):
+        # TODO: this is busted. it returns hundreds of thousands of results most of time, because
+        # builds.number isn't unique.
         return self.db.execute(
             sa.text("select buildrequests.id from buildrequests join builds ON buildrequests.id=builds.brid where builds.number=:buildnumber"),
             buildnumber=buildnumber,
