1659218c322646b1e0ae45a0edcb0af443562694
Ben Hearsum <bhearsum@mozilla.com>
2015-09-10 10:57:02 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name="bbb",
-    version="1.4.2",
+    version="1.5",
     description="Buildbot <-> Taskcluster Bridge",
     author="Mozilla Release Engineering",
     packages=["bbb", "bbb.schemas"],
