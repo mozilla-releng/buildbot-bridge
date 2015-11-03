7bcd592a29a4fff5c002426969ce177f31d0a187
Dustin J. Mitchell <dustin@mozilla.com>
2015-11-03 14:07:54 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name="bbb",
-    version="1.5.1",
+    version="1.5.2",
     description="Buildbot <-> Taskcluster Bridge",
     author="Mozilla Release Engineering",
     packages=["bbb", "bbb.schemas"],
