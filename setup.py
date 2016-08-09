97b7b31f9b4fd750ef9e36bc82e46e2b2296699f
Rail Aliiev <rail@mozilla.com>
2016-08-09 12:44:22 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 
 setup(
     name="bbb",
-    version="1.5.18",
+    version="1.5.19",
     description="Buildbot <-> Taskcluster Bridge",
     author="Mozilla Release Engineering",
     packages=["bbb", "bbb.schemas"],
