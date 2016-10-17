147db1bd796cd39554a8088a0d4fc24468b47ba1
Rail Aliiev <rail@mozilla.com>
2016-10-17 11:02:31 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="bbb",
-    version="1.5.21",
+    version="1.5.22",
     description="Buildbot <-> Taskcluster Bridge",
     author="Mozilla Release Engineering",
     packages=["bbb", "bbb.schemas"],
